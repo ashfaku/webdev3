7c841f998f57ec945c973feaf60f4b13522c3740
Sofia Zevallos <sofiazevallos50@gmail.com>
2025-04-13 16:32:45 +0100

    Updated credits,js to use the container class

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,22 +1,19 @@
+// Credits.js
+
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Credits = (props) => {
-
-  useEffect(() => {
-    document.title = "Credits";
-  }, []); 
-
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState("");
 
-  const submitCredit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     const newCredit = {
-      id: props.credits.length + 1,
+      id: Math.random(),
       description,
       amount: parseFloat(amount).toFixed(2),
-      date: new Date().toISOString(),
+      date: new Date().toISOString().split("T")[0],
     };
     props.addCredit(newCredit);
     setDescription("");
@@ -24,23 +21,41 @@ const Credits = (props) => {
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Credits</h1>
-      <div>Account Balance: {props.accountBalance}</div>
+
       <ul>
         {props.credits.map((credit) => (
-          <li style={{listStyleType: 'none'}} key={credit.id}>{credit.amount} - {credit.description} - {credit.date.split("T")[0]}</li>
+          <li key={credit.id}>
+            {credit.amount} - {credit.description} - {credit.date}
+          </li>
         ))}
       </ul>
-      <form onSubmit={submitCredit}>
-        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Enter description" required />
-        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount" step="0.01" required />
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Description"
+          required
+        />
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          placeholder="Amount"
+          step="0.01"
+          required
+        />
         <button type="submit">Add Credit</button>
       </form>
+
       <br />
       <Link to="/">Return to Home</Link>
     </div>
   );
-}
+};
 
 export default Credits;
+
